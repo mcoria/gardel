d2ba644305a8e9dc4b9c8d0deb9496b995af5877
Mauricio Coria <coriamauricio@gmail.com>
2025-05-16 10:12:25 -0300

    Add PERFT level 2 test for KNRBvKNRB endgame scenario
    Introduced a new unit test `test_POS1_PERFT_L2` to validate move counts and child nodes at depth 2 for the KNRB vs. KNRB chess endgame. This ensures accuracy of the perft implementation in this specific scenario.

--- src/test/java/net/chesstango/gardel/minchess/KNRBvKNRBTest.java
+++ src/test/java/net/chesstango/gardel/minchess/KNRBvKNRBTest.java
@@ -50,4 +50,38 @@ public class KNRBvKNRBTest extends AbstractPerftTest {
         assertEquals(24, result.getMovesCount());
         assertEquals(24, result.getTotalNodes());
     }
+
+    @Test
+    public void test_POS1_PERFT_L2() {
+        MinChess game = createGame(POSITION);
+
+        PerftResult result = perft.start(game, 2);
+
+        assertEquals(21, result.getChildNode(Square.a1, Square.b2));
+        assertEquals(21, result.getChildNode(Square.b3, Square.a3));
+        assertEquals(21, result.getChildNode(Square.b3, Square.b1));
+        assertEquals(21, result.getChildNode(Square.b3, Square.b2));
+        assertEquals(21, result.getChildNode(Square.b3, Square.b4));
+        assertEquals(17, result.getChildNode(Square.b3, Square.b5));
+        assertEquals(3, result.getChildNode(Square.b3, Square.b6));
+        assertEquals(17, result.getChildNode(Square.b3, Square.b7));
+        assertEquals(21, result.getChildNode(Square.b3, Square.b8));
+        assertEquals(20, result.getChildNode(Square.c3, Square.a2));
+        assertEquals(20, result.getChildNode(Square.c3, Square.a4));
+        assertEquals(20, result.getChildNode(Square.c3, Square.b1));
+        assertEquals(15, result.getChildNode(Square.c3, Square.b5));
+        assertEquals(20, result.getChildNode(Square.c3, Square.d1));
+        assertEquals(19, result.getChildNode(Square.c3, Square.d5));
+        assertEquals(19, result.getChildNode(Square.c3, Square.e2));
+        assertEquals(22, result.getChildNode(Square.e4, Square.d4));
+        assertEquals(23, result.getChildNode(Square.e4, Square.e3));
+        assertEquals(23, result.getChildNode(Square.e4, Square.f3));
+        assertEquals(21, result.getChildNode(Square.e4, Square.f4));
+        assertEquals(21, result.getChildNode(Square.g2, Square.f1));
+        assertEquals(21, result.getChildNode(Square.g2, Square.f3));
+        assertEquals(21, result.getChildNode(Square.g2, Square.h1));
+        assertEquals(6, result.getChildNode(Square.g2, Square.h3));
+        assertEquals(24, result.getMovesCount());
+        assertEquals(454, result.getTotalNodes());
+    }
 }
