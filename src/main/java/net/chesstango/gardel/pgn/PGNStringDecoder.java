62d3e387bcbc6cb2de8ca39c56241623936e0535
Mauricio Coria <coriamauricio@gmail.com>
2026-01-07 17:39:39 -0300

    Add `decodePGN(String)` method to simplify PGN decoding from strings

--- src/main/java/net/chesstango/gardel/pgn/PGNStringDecoder.java
+++ src/main/java/net/chesstango/gardel/pgn/PGNStringDecoder.java
@@ -1,25 +1,17 @@
 package net.chesstango.gardel.pgn;
 
 
-import net.chesstango.gardel.fen.FEN;
 import net.chesstango.gardel.internal.antlr4.PGNLexer;
 import net.chesstango.gardel.internal.antlr4.PGNParser;
-import net.chesstango.gardel.move.SANDecoder;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 /**
@@ -36,6 +28,10 @@ public class PGNStringDecoder {
         // 1. Create a CharStream from the input source
         CharStream input = CharStreams.fromStream(inputStream);
         return decodePGNs(input);
+    }
+
+    public PGN decodePGN(String input) {
+        return decodePGN(CharStreams.fromString(input));
     }
 
     Stream<PGN> decodePGNs(CharStream input) {
